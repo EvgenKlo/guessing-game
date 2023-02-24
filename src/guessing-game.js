f00c9c883bc4b8ef8edbd0e50a1da87e29e0cd76
EvgenKlo <klokovevgen@gmail.com>
2023-02-24 13:18:38 +0500

    feat:done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.result;
+        this.max;
+        this.min;
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
+        this.result = Math.round(this.max / 2);
     }
 
     guess() {
-
+        return this.result;
     }
 
     lower() {
-
+        this.max = this.result;
+        this.result = Math.round((this.max + this.min) / 2);
     }
 
     greater() {
-
+        this.min = this.result;
+        this.result = Math.round((this.max + this.min) / 2);
     }
 }
 
